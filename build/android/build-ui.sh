9b97426c309c2d3b48bef97411a7b61403795972
Arun Krishna <arun007coder@gmail.com>
2025-01-12 14:07:03 +0530

    Update build-ui.sh

--- build/android/build-ui.sh
+++ build/android/build-ui.sh
@@ -3,16 +3,19 @@
 LOCAL_PATH=`dirname $0`
 LOCAL_PATH=`cd $LOCAL_PATH && pwd`
 
+SDL_PATH=~/androidsdl
+BOCHS_PATH=~/bochs
+
 if [ \! -d BochsLauncher/bochsApp/src ] ; then
-   ln -s ../../../../../androidsdl/project/src BochsLauncher/bochsApp/src
+   ln -s "$SDL_PATH"/project/src BochsLauncher/bochsApp/src
 fi
 
 if [ \! -d BochsLauncher/bochsApp/res ] ; then
-   ln -s ../../../../../androidsdl/project/res BochsLauncher/bochsApp/res
+   ln -s "$SDL_PATH"/project/res BochsLauncher/bochsApp/res
 fi
 
 if [ \! -d BochsLauncher/app/src/main/jniLibs ] ; then
-   ln -s ../../../../../../../androidsdl/project/libs BochsLauncher/app/src/main/jniLibs
+   ln -s "$SDL_PATH"/project/libs BochsLauncher/app/src/main/jniLibs
 fi
 
 cd BochsLauncher
