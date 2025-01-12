eb41b82c099a4ecd696778806234a82d046c33e6
Arun Krishna <arun007coder@gmail.com>
2025-01-12 11:40:26 +0530

    Update build_android.sh

--- build_android.sh
+++ build_android.sh
@@ -9,11 +9,15 @@ mkdir "$SCROOT" "$ZIPDIR" "$TOOLDIR"
 # download android ndk
 init_android_ndk() {
     wget -O "$ZIPDIR"/ANNDK.zip https://dl.google.com/android/repository/android-ndk-r27c-linux.zip
-    unzip "$ZIPDIR"/ANNDK.zip -d "$TOOLDIR"/a_ndk/
+    unzip "$ZIPDIR"/ANNDK.zip -d "$TOOLDIR"/andk/
     echo "Android NDK unzipped"
 }
 
 # download platform sdk tools
 init_android_sdk() {
     wget -O "$ZIPDIR"/ANPTOOLS.zip https://dl.google.com/android/repository/platform-tools-latest-linux.zip
+    unzip "$ZIPDIR"/ANPTOOLS.zip -d "$TOOLDIR"/asdk_tools/
+    echo "Android platform tools unzipped"
+
+    
 }
