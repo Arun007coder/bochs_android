dfee831abe3519ba2cae5a8e8b3a675abcd6a2ba
Arun Krishna <arun007coder@gmail.com>
2025-01-12 14:19:24 +0530

    Update build-core.sh

--- build/android/build-core.sh
+++ build/android/build-core.sh
@@ -3,7 +3,7 @@
 LOCAL_PATH=`dirname $0`
 LOCAL_PATH=`cd $LOCAL_PATH && pwd`
 SDL_PATH=~/androidsdl
-BOCHS_PATH=~/bochs
+BOCHS_PATH=~/bochs_android
 
 if [ \! -d bochs/bochs ] ; then
    ln -s "$BOCHS_PATH" bochs
