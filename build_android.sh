865c15a3015307a9fcef2158322c45d84b46c7b4
Arun Krishna <arun007coder@gmail.com>
2025-01-12 11:49:17 +0530

    Update build_android.sh

--- build_android.sh
+++ build_android.sh
@@ -4,6 +4,8 @@ SCROOT=~/bochs/
 ZIPDIR="$SCROOT"/ZIPS/
 TOOLDIR="$SCROOT"/TOOLS/
 
+ANDROID_HOME="$TOOLDIR"/SDK
+
 mkdir "$SCROOT" "$ZIPDIR" "$TOOLDIR"
 
 # download android ndk
@@ -16,9 +18,9 @@ init_android_ndk() {
 # download platform sdk tools
 init_android_sdk() {
     wget -O "$ZIPDIR"/ANPTOOLS.zip https://dl.google.com/android/repository/platform-tools-latest-linux.zip
-    unzip "$ZIPDIR"/ANPTOOLS.zip -d "$TOOLDIR"/asdk_tools/
+    unzip "$ZIPDIR"/ANPTOOLS.zip -d "$TOOLDIR"/
     echo "Android platform tools unzipped"
-    "$TOOLDIR"/asdk_tools/bin/sdkmanager "platforms;android-25" --sdk_root="$TOOLDIR"/SDK/
-    "$TOOLDIR"/asdk_tools/bin/sdkmanager update
+    "$TOOLDIR"/tools/bin/sdkmanager --sdk_root="$ANDROID_HOME" "platforms;android-25" "build-tools;27.0.3" "platform-tools"
+    "$TOOLDIR"/tools/bin/sdkmanager update
     
 }
